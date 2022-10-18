3028c4451a47f4006d2f86413d00f727922faf88
Nick Jackson <nickdjackson@msn.com>
2022-10-18 16:06:10 -0400

    second

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,12 @@
-import functions from "firebase-functions"
-
+import functions from 'firebase-functions'
 import express from 'express'
+import { createUser } from './src/users.js'
 
 const app = express()
 
 app.get('/test', (req, res) => res.send('Our first cloud API works, MF!'))
+
+app.post('/users', createUser)
 
 export const api = functions.https.onRequest(app)
 
