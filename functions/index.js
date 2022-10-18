a722576e95c6b9ba877836637a25b3a6b7ae4e2f
Nick Jackson <nickdjackson@msn.com>
2022-10-18 16:44:34 -0400

    third

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,13 @@
 import functions from 'firebase-functions'
 import express from 'express'
-import { createUser } from './src/users.js'
+import { createUser, getAllUsers } from './src/users.js'
 
 const app = express()
+app.use(express.json())
 
 app.get('/test', (req, res) => res.send('Our first cloud API works, MF!'))
+
+app.get('/users', getAllUsers)
 
 app.post('/users', createUser)
 
--- functions/src/users.js
+++ functions/src/users.js
@@ -6,3 +6,16 @@ export async function createUser(req, res) {
         .catch(err => res.status(500).send({ success: false, message: err}));
     res.status(201).send({ success: true, message: 'User created: ' + doc.id});
 }
+
+export async function getAllUsers(req, res) {
+    const db = dbConnect();
+    const collection = await db.collection('users').get()
+        .catch(err => res.status(500).send({ success: false, message: err}))
+    const users = collection.docs.map(doc => {
+        let user = doc.data()
+        user.uid = doc.id
+        return user
+    })
+    res.send(users)
+}
+
