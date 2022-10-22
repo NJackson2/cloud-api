39e549e359574101426f21a085a461feaeb0ebee
Nick Jackson <nickdjackson@msn.com>
2022-10-22 15:01:34 -0400

    second

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,19 @@
-import functions from 'firebase-functions'
+import functions from 'firebase-functions' // these are the cloud functions
 import express from 'express'
-import { createUser, getAllUsers } from './src/users.js'
+import { createUser, getAllUsers, updateUser } from './src/users.js'
 
 const app = express()
 app.use(express.json())
 
 app.get('/test', (req, res) => res.send('Our first cloud API works, MF!'))
-
 app.get('/users', getAllUsers)
 
 app.post('/users', createUser)
 
+app.patch('/users/: uid', updateUser)
+
 export const api = functions.https.onRequest(app)
+// deploying our api to the cloud, listening through routes. 
+// We use api in the firebase.json file
 
 
